cc3628c6bcfa7d8101022c5c8af0ed167fc2faf2
1Heyzeus <148654475+1Heyzeus@users.noreply.github.com>
2023-11-02 19:07:00 +0000

    Add user input for grid size

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
     genGrid(16);
-    console.log('Hi')
+
+    let btn_popup = document.querySelector('#select');
+    btn_popup.addEventListener('click', () =>{
+        let size = getSize();
+        genGrid(size);
+    })
 })
 
 function genGrid(size){
@@ -14,5 +19,18 @@ function genGrid(size){
     for(let i=0;i<numDivs;i++){
         let div = document.createElement('div');
         grid.insertAdjacentElement('beforeend', div);
+    }
+}
+
+function getSize(){
+    let input = window.prompt('Please enter a size for the board');
+    let message = document.querySelector('#message');
+    if(input === ''){
+        message.textContent = 'Please enter a number';
+    } else if (input <=0 || input > 100){
+        message.textContent = 'Please choose a number between 1 and 100';
+    } else {
+        message.textContent = 'Thank you!';
+        return input;
     }
 }
